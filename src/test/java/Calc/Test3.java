b887a01586b943efaa103504dc1994b882afe25e
Olenu <34704079+Olenu@users.noreply.github.com>
2018-04-22 12:34:42 +0500

    Create Test3.java

---
+++ src/test/java/Calc/Test3.java
@@ -0,0 +1,27 @@
+package Calc;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class korporateCalcTest {
+
+    korporateCalc testingClass;
+
+  @BeforeEach
+   void setUp() {
+       testingClass = new korporateCalc();
+    }
+
+    @AfterEach
+    void tearDown() {
+        testingClass = null;
+    }
+
+    @Test
+    void getP() {
+              assertEquals("391.02",testingClass.getP(10, 14, 52, 55));
+           }
+    } 
